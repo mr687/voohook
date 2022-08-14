d6e34dfe148882b425170affcd1f943f6d6e512c
mr687 <davinomoehdanino@gmail.com>
2022-08-14 10:24:42 +0700

    deploy to heroku

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,7 +5,6 @@ const config = {
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || '6379',
   password: process.env.REDIS_PASSWORD || null,
-  db: 2,
   prefix: 'vhk_',
 }
 
